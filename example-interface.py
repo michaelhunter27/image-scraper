be8a1054a3f1d4553a6305f75154d9b175a5f34c
michaelhunter27 <michael.max.hunter27@gmail.com>
2024-02-21 14:47:47 -0800

    v1.0

--- example-interface.py
+++ example-interface.py
@@ -2,17 +2,21 @@ import os
 import zmq
 import subprocess
 
-# filepath to microservice python code
+# filepath to microservice python code (relative or absolute)
 MICROSERVICE_FILEPATH = "./microservice/microservice.py"
+
+# image save location (relative filepath)
+IMAGE_FILEPATH = "./agate_beach.jpg"
 
 # starts the microservice in another process
 subprocess.Popen("Python " + MICROSERVICE_FILEPATH)
 
 # set up PyZMQ socket
+PORT = "5555"
 context = zmq.Context()
 print("connecting to server")
 socket = context.socket(zmq.REQ)
-socket.connect("tcp://localhost:5555")
+socket.connect("tcp://localhost:" + PORT)
 
 # Main loop
 input_string = ""
@@ -24,12 +28,22 @@ while input_string != "2":
     input_string = input()
 
     if input_string == "1":
-        print("requesting image!")
+        # request image
+        print("requesting image")
         socket.send(b"cam_image")
+
+        # receive image
         image = socket.recv()
-        print("image received!")
-        print(image.decode())
-        os.startfile(image.decode())
+        print("image received")
+
+        # save image to file
+        f = open(IMAGE_FILEPATH, 'wb')
+        bytes_to_write = bytearray(image)
+        f.write(bytes_to_write)
+        f.close()
+
+        # opens image
+        os.startfile("agate_beach.jpg")
 
 # sends message to stop microservice
 socket.send(b"quit")
--- microservice/microservice.py
+++ microservice/microservice.py
@@ -1,41 +1,51 @@
 import os
 import urllib.request
-
-import requests.exceptions
 import zmq
 import shutil
+import pathlib
 
-# URL of image to fetch - DO NOT CHANGE
+# URL of image to fetch
 URL = "https://camstills.cdn-surfline.com/wc-agatebeachor/latest_full.jpg"
 
-# filepath where image will be saved
-os.chdir("C:/Users/micha/Desktop/image-scraper/microservice/")
-current_directory = os.getcwd()
-image_filepath = current_directory + "/" + "agate_beach.jpg"
-cached_image = current_directory + "/" + "cache.jpg"
-image_to_send = image_filepath
+# directory where image will be saved (if different from the directory this file is in)
+WORKING_DIRECTORY = "./microservice"
+os.chdir(WORKING_DIRECTORY)
+
+# filepath where downloaded image will be saved
+DOWNLOAD_IMAGE_FILEPATH = "./agate_beach.jpg"
+
+# filepath where cached image will be saved
+CACHED_IMAGE_FILEPATH = "./cache.jpg"
 
 # set up PyZMQ socket
+PORT = "5555"
 context = zmq.Context()
 socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
-print("microservice connected and ready")
+socket.bind("tcp://*:" + PORT)
 
 while True:
     message = socket.recv()
     if message.decode() == "cam_image":
-        print("microservice received request!")
+        image_to_send = DOWNLOAD_IMAGE_FILEPATH
         try:
-            urllib.request.urlretrieve(URL, image_filepath)
-            image_to_send = image_filepath
-            shutil.copy(image_filepath, cached_image)
-        except:
-            print("There was an error! Sending cached image")
-            image_to_send = cached_image
+            # fetch image
+            urllib.request.urlretrieve(URL, DOWNLOAD_IMAGE_FILEPATH)
+            # save to cache for future use
+            shutil.copy(DOWNLOAD_IMAGE_FILEPATH, CACHED_IMAGE_FILEPATH)
+        except Exception as e:
+            print("There was an error:", e, "\nSending cached image")
+            # send cached image if there was an error
+            image_to_send = CACHED_IMAGE_FILEPATH
         finally:
-            socket.send(image_to_send.encode())
-    else:
-        print(message.decode())
-        print("microservice quitting!")
+            f = open(image_to_send, 'rb')
+            message_to_send = bytearray(f.read())
+            socket.send(message_to_send)
+            f.close()
+
+            # remove downloaded image
+            if os.path.isfile(DOWNLOAD_IMAGE_FILEPATH):
+                pathlib.Path(DOWNLOAD_IMAGE_FILEPATH).unlink()
+
+    elif message.decode() == "quit":
         socket.send(b"quitting")
         break
